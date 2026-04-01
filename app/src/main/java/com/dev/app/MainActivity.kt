3ede9e8cf0a013c74b5fd5b93099f3c771a07638
francis-xie <1159394241@qq.com>
2026-04-01 12:44:24 +0800

    初始

---
+++ app/src/main/java/com/dev/app/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.dev.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.dev.app.ui.theme.DevAppTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      DevAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(text = "Hello $name!", modifier = modifier)
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  DevAppTheme { Greeting("Android") }
+}
